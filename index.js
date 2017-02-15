f5f08414391bbbaeb1ccc3e14bb2b6d01e8e43d6
unknown <phuengton.ch.57@ubu.ac.th>
2017-02-15 23:17:13 +0700

    test

--- index.js
+++ index.js
@@ -2,52 +2,54 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+/*eslint-disable */
+// var env = require('dotenv').config({ path: __dirname + '/.env' })
+/*eslint-enable */
 
 app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-
-app.post('/', (req, res) => {
-	var text = req.body.events[0].message.text
-	var sender = req.body.events[0].source.userId
-	var replyToken = req.body.events[0].replyToken
-	console.log(text, sender, replyToken)
-	console.log(typeof sender, typeof text)
-	// console.log(req.body.events[0])
-	sendText(sender, text)
-
-	res.sendStatus(200)
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
+  res.sendStatus(200)
 })
 
-function sendText(sender, text) {
-	let data = {
+function sendText (sender, text) {
+  let data = {
     to: sender,
     messages: [
       {
         type: 'text',
-        text: this.text
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
       }
     ]
   }
-	request({
-	headers: {
-		'Content-Type': 'application/json',
-		'Authorization': 'Bearer KFifFHoe92Tndot2UA3hCtijv9tbHB6q81A30ItcJh+Xq6Dc3ScUBqoU41SsIPVQpViQqJBgtXtxsmqTrYpy4BQFoNTdO3ijn7a0CC27fKVsj3tqynsWT3rDGrM5bEwaCbpURfYc5C6FGTQo/sdPdAdB04t89/1O/w1cDnyilFU='
-	},
-	url: 'https://api.line.me/v2/bot/message/push',
-	method: 'POST',
-	body: data,
-	json: true
-	}, function (err, res, body) {
-		if (err) console.log('error')
-		if (res) console.log('success')
-		if (body) console.log(body)
-	})
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer KFifFHoe92Tndot2UA3hCtijv9tbHB6q81A30ItcJh+Xq6Dc3ScUBqoU41SsIPVQpViQqJBgtXtxsmqTrYpy4BQFoNTdO3ijn7a0CC27fKVsj3tqynsWT3rDGrM5bEwaCbpURfYc5C6FGTQo/sdPdAdB04t89/1O/w1cDnyilFU='
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
 }
-
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
